6f920329b0b34717e3c44a5c74c3cab65df4b40e
Neetu Chauhan <neetuchauhan0421@gmail.com>
2020-06-30 21:55:21 +0530

    Updates

---
+++ precache-manifest.561fb6cd6e197b333dc0a165864432aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a0f9e77703f49d95a5bdaa5ae772a9",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "36c8bd21861bbaac0d7a",
+    "url": "/myportfolio/static/css/main.5e2e32ae.chunk.css"
+  },
+  {
+    "revision": "69fdcb8cb12771a7f3fe",
+    "url": "/myportfolio/static/js/2.2f2abcee.chunk.js"
+  },
+  {
+    "revision": "8ba122882028b763643af6be6ad5e72e",
+    "url": "/myportfolio/static/js/2.2f2abcee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c8bd21861bbaac0d7a",
+    "url": "/myportfolio/static/js/main.22063c69.chunk.js"
+  },
+  {
+    "revision": "beb4593db90054a6b6c5",
+    "url": "/myportfolio/static/js/runtime-main.abd1b5b3.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/myportfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "9ec582cac18becd9e9138dc9335f9007",
+    "url": "/myportfolio/static/media/neetu.9ec582ca.png"
+  }
+]);
